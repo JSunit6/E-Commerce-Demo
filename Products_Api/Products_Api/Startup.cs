f4d3265ae9147ce598237e793454a0bfc376f1c0
Sunit Kajarekar <skajarekar@hawk.iit.edu>
2020-10-06 19:26:31 -0400

    Added Authorization and Updated Authentication Controller to include Authorization code and Login Code

--- Products_Api/Products_Api/Startup.cs
+++ Products_Api/Products_Api/Startup.cs
@@ -32,10 +32,12 @@ namespace Products_Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApplicationSettings>(Configuration.GetSection("ApplicationSettings"));
             services.AddDbContext<ShoppingDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddDbContext<AppUserContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddTransient<IRepository, SqlRepository>();
             services.AddIdentity<AppUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<AppUserContext>();
 
             services.AddControllers();
