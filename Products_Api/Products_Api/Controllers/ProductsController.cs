e2962002f6c1f37882e9667d39b325ab3a9e4d97
Sunit Kajarekar <skajarekar@hawk.iit.edu>
2020-10-09 18:17:11 -0400

    Updated repositories, added Product controller and ProductQueryParameter class

--- Products_Api/Products_Api/Controllers/ProductsController.cs
+++ Products_Api/Products_Api/Controllers/ProductsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Products_Api.Classes;
+using Products_Api.Models;
 using Products_Api.Repository;
 
 namespace Products_Api.Controllers
@@ -17,6 +20,72 @@ namespace Products_Api.Controllers
         public ProductsController(IRepository repository)
         {
             this._repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts([FromQuery] ProductQueryParameters queryParameters)
+        {
+            IQueryable<Products> products = (IQueryable<Products>) await this._repository.ViewAllProducts();
+
+            if (string.IsNullOrEmpty(queryParameters.Brand))
+            {
+                products = products.Where(
+                        product => product.ProductBrand == queryParameters.Brand 
+                    );
+            }
+
+            if (string.IsNullOrEmpty(queryParameters.ProductName))
+            {
+                products = products.Where(
+                        product => product.ProductName.Equals(queryParameters.ProductName)
+                    );
+            }
+
+            if (queryParameters.MinPrice != 0 || queryParameters.MaxPrice!=decimal.MaxValue)
+            {
+                products = products.Where(
+                        product => product.ProductPrice >= queryParameters.MinPrice && product.ProductPrice <= queryParameters.MaxPrice
+                    );
+            }
+            return Ok(products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct([FromRoute] int id)
+        {
+            var product = await this._repository.ViewProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostProduct([FromBody] Products product)
+        {
+            if (ModelState.IsValid)
+            {
+                int productAdded = await this._repository.AddProduct(product);
+
+                if (productAdded > 0)
+                {
+                    return CreatedAtAction(
+                        "GetProduct",
+                        new { id = product.ProductId },
+                        product
+                    );
+                }
+
+                else
+                {
+                    return BadRequest("Error Adding Product");
+                }
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }
--- Products_Api/Products_Api/Repository/SqlRepository.cs
+++ Products_Api/Products_Api/Repository/SqlRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Products_Api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,27 @@ namespace Products_Api.Repository
 {
     public class SqlRepository: IRepository
     {
+        private ShoppingDBContext _context;
+
+        public SqlRepository(ShoppingDBContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<int> AddProduct(Products product)
+        {
+            await this._context.Products.AddAsync(product);
+            return await this._context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Products>> ViewAllProducts()
+        {
+            return  await this._context.Products.ToArrayAsync<Products>();
+        }
+
+        public async Task<Products> ViewProductById(int productId)
+        {
+            return await this._context.Products.FindAsync(productId);
+        }
     }
 }
