7436d4150538601be18147fb7176c7dc2f717125
Sunit Kajarekar <skajarekar@hawk.iit.edu>
2020-11-02 19:04:48 -0500

    Configured Get all products component

--- ProductsApplication/src/app/app-routing.module.ts
+++ ProductsApplication/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AddproductsComponent } from './components/addproducts/addproducts.component';
 import { AddproductcategoriesComponent } from './components/addproductcategories/addproductcategories.component';
+import { GetallproductsComponent } from './components/getallproducts/getallproducts.component';
 
 const routes: Routes = [
   { path:'register', component: RegisterComponent, data:{ title:'Sign Up' } },
   { path: 'login', component: LoginComponent, data:{ title:'Sign In' } },
   { path: 'addProducts', component: AddproductsComponent, data:{ title:'Add Product' } },
-  { path: 'addProductCategories', component: AddproductcategoriesComponent, data:{ title: 'Add Product Category'}}
+  { path: 'addProductCategories', component: AddproductcategoriesComponent, data:{ title: 'Add Product Category'}},
+  { path: 'getallproducts', component: GetallproductsComponent, data: { title: 'Get All Products'}}
 ];
 
 @NgModule({
